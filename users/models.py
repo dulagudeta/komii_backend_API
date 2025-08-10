93c00006484cee1bf3904192b882dc6737d3eb7e
Dula Gudeta <dulagudeta22@gmail.com>
2025-08-10 14:40:13 +0300

    user model created

---
+++ users/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+class User(AbstractUser):
+    RESIDENT = 'resident'
+    STAFF = 'staff'
+    ADMIN = 'admin'
+    ROLE_CHOICES = [
+        (RESIDENT, 'Resident'),
+        (STAFF, 'Staff'),
+        (ADMIN, 'Admin'),
+    ]
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default=RESIDENT)
+
+    def is_resident(self):
+        return self.role == self.RESIDENT
+
+    def is_staff_member(self):
+        return self.role == self.STAFF
+
+    def is_admin(self):
+        return self.role == self.ADMIN

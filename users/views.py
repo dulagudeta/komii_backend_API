c3c98bee0974d5d5448882ff5c5a86daa2ff5700
Dula Gudeta <dulagudeta22@gmail.com>
2025-08-11 10:46:10 +0300

    view and urls updated for user registeratin

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
+from django.contrib.auth import get_user_model
+from .serializers import RegisterSerializer, UserSerializer
+from rest_framework.permissions import AllowAny
+
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]  # Anyone can register
+    serializer_class = RegisterSerializer
+
 
 # Create your views here.

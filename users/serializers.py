b6e83fbb74fcc7013fb66b55d6b994ab8704cea0
Dula Gudeta <dulagudeta22@gmail.com>
2025-08-11 10:43:05 +0300

    Create serializers.py

---
+++ users/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    """For returning user data."""
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'role']
+
+class RegisterSerializer(serializers.ModelSerializer):
+    """For user registration."""
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'role', 'password', 'password2']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Passwords do not match."})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            role=validated_data.get('role', User.RESIDENT),
+            password=validated_data['password']
+        )
+        return user

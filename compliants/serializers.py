9c6527ae98a31c0e08ffb1fe972154a6cee80e11
Dula Gudeta <dulagudeta22@gmail.com>
2025-08-24 13:17:12 +0300

    Update serializers.py

--- compliants/serializers.py
+++ compliants/serializers.py
@@ -1,4 +1,3 @@
-# complaints/serializers.py
 from rest_framework import serializers
 from .models import Complaint
 
@@ -9,9 +8,8 @@ class ComplaintSerializer(serializers.ModelSerializer):
         read_only_fields = ['reported_by', 'created_at', 'updated_at']
 
     def update(self, instance, validated_data):
-        # Only certain roles can change status
         request = self.context.get('request')
         if 'status' in validated_data:
-            if not request.user.is_staff:  # or custom role check
+            if not request.user.is_staff:  
                 raise serializers.ValidationError("You are not allowed to change the status.")
         return super().update(instance, validated_data)

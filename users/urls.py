0512b4d398248bc7441d6df5d02b51f937e6684a
Dula Gudeta <dulagudeta22@gmail.com>
2025-08-27 20:12:44 +0300

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('users/', RegisterView.as_view(), name='user_list'),
     path('users/<int:pk>/', RegisterView.as_view(), name='user_detail'),
-
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]

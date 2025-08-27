1d30b567f4970899ee310bd528d2fed1ede5283e
Dula Gudeta <dulagudeta22@gmail.com>
2025-08-27 20:12:08 +0300

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('users/', RegisterView.as_view(), name='user_list'),
-    
+    path('users/<int:pk>/', RegisterView.as_view(), name='user_detail'),
+
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]

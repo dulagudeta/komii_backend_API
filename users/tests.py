0c63e0c571c8aac7e404bb6bef6943355607d68a
Dula Gudeta <dulagudeta22@gmail.com>
2025-09-04 12:02:30 +0300

    Update tests.py
    test_user_detail

--- users/tests.py
+++ users/tests.py
@@ -19,3 +19,9 @@ class CompliantTests(TestCase):
             'username': 'testuser2',
             'password': 'testpassword2'
         })
+
+    def test_user_detail(self):
+        user_id = response.data['id']
+        response = self.client.get(f'/api/users/{user_id}/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['username'], 'testuser2')

f32be2fbf227910eeae0b64821dbf37804f80c20
Dula Gudeta <dulagudeta22@gmail.com>
2025-09-04 12:01:28 +0300

    Update tests.py
    user_can_view_user_list

--- users/tests.py
+++ users/tests.py
@@ -8,3 +8,9 @@ class CompliantTests(TestCase):
             'password': 'testpassword'
         })
         self.assertEqual(response.status_code, 201)
+
+    def user_can_view_user_list(self):
+        response = self.client.get('/api/users/')
+        self.assertEqual(response.status_code, 200)
+    
+    

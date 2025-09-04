76f9cedbbfa9b4b2c602c611f43aac101e26b924
Dula Gudeta <dulagudeta22@gmail.com>
2025-09-04 12:00:53 +0300

    Update tests.py
    user_can_register test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+class CompliantTests(TestCase):
+    def user_can_register(self):
+        response = self.client.post('/api/register/', {
+            'username': 'testuser',
+            'password': 'testpassword'
+        })
+        self.assertEqual(response.status_code, 201)
